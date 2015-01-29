d96f0f9fa4accfb52e5babaf69a04cd3e9dedbd4
wsmits <wsmits@PC-WESLEY>
2015-01-29 12:41:14 +0100

    Added isDeleted property to record

--- lib/GO/Core/Controller/AbstractRESTController.php
+++ lib/GO/Core/Controller/AbstractRESTController.php
@@ -289,7 +289,7 @@ abstract class AbstractRESTController extends AbstractObject {
 	 */
 	protected function renderModel(AbstractModel $model, $returnAttributes = null) {
 
-		if($model->isDeleted()){
+		if(!$model->isDeleted()){
 //		if(App::request()->getMethod() != 'DELETE'){
 	
 			if (App::request()->getMethod() == 'GET' && isset($model->modifiedAt)) {
